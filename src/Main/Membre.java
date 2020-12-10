edb33dd70396f06d83cf890e3f9003cb1cc06b03
DesjardinsL <DesjardinsL@example.com>
2020-12-09 19:54:56 -0500

    Création classe: Employe, Film, Membre

---
+++ src/Main/Membre.java
@@ -0,0 +1,56 @@
+package Main;
+
+public class Membre {
+	
+	//Attributs
+		private String nom;
+		private String noTelephone;
+		private int noCarte;
+		private int codeSecret;
+		
+		
+	//Constructeur
+		public Membre(String nom, String noTelephone, int noCarte, int codeSecret) {
+			this.nom = nom;
+			this.noTelephone = noTelephone;
+			this.noCarte = noCarte;
+			this.codeSecret = codeSecret;
+		}
+		
+	
+	//Getters (Accesseurs)
+		public String getNom(){
+			return this.nom;
+		}
+		
+		public String getNoTelepone(){
+			return this.noTelephone;
+		}
+	
+		public int getNoCarte(){
+			return this.noCarte;
+		}
+	
+		public int getCodeSecret(){
+			return this.codeSecret;
+		}
+		
+		
+	//Setters (Mutateurs)
+		public void setNom(String nom){
+			this.nom = nom;
+		}
+		
+		public void setNoTelepone(String noTelephone){
+			this.noTelephone = noTelephone;
+		}
+	
+		public void setNoCarte(int noCarte){
+			this.noCarte = noCarte;
+		}
+	
+		public void setCodeSecret(int codeSecret){
+			this.codeSecret = codeSecret;
+		}
+	
+}
